d411be79e84cb067a1422aae050edcdf8e0b4119
Tony <antonas19981@gmail.com>
2025-10-01 22:34:38 +0300

    #8 H\A

---
+++ src/app/components/forms/formInterface.ts
@@ -0,0 +1,8 @@
+export interface UserForm{
+  email : string;
+  username : string;
+  birthdate : string;
+  password : string;
+  confirmPassword : string
+  hobbies : string;
+}
--- src/app/components/forms/forms.ts
+++ src/app/components/forms/forms.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
 import { minAgeValidator } from '../../validators/min-age.validator';
 import { correctPassValidator } from '../../validators/correctPassValidator';
 import { passwordMatchValidator } from '../../validators/passConfirmValidator';
+import { UserForm } from './formInterface';
+import { ChangeDetectorRef } from '@angular/core';
+
 
 
 @Component({
   selector: 'app-forms',
   imports: [ReactiveFormsModule],
   templateUrl: './forms.html',
-  styleUrl: './forms.scss'
+  styleUrl: './forms.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class Forms {
 myForm = new FormGroup(
   {
@@ -23,15 +28,49 @@ myForm = new FormGroup(
  },
  {validators : passwordMatchValidator}
 );
+constructor(private cdr: ChangeDetectorRef) {}
 
- submittedData: any = null;
+ submittedData: UserForm[] = [];
+ hobbyArr : string[] = [];
+ 
+  addHobby(){
+   const hobby = this.myForm.controls['hobbies'].value
+  if(hobby){
+    this.hobbyArr.push(hobby);
+    this.myForm.controls['hobbies'].reset(); 
+  }
+  }
+  deleteHobby(){
+    if (this.hobbyArr.length == 0){
+      return
+    }
+    this.hobbyArr.pop();
+  }
+
+
  submitForm() {
     if (this.myForm.invalid) {
       this.myForm.markAllAsTouched();
       return;
     }
 
-    this.submittedData = { ...this.myForm.value }; 
-    console.log('Сохранённые данные:', this.submittedData);
+    const formValue = {
+      ...this.myForm.value,
+      hobbies : this.hobbyArr.join(', ')
+    };
+
+    this.submittedData.push(formValue as UserForm);
+    console.log('Сохранено:', this.submittedData);
+
+    this.myForm.reset({
+  email: '',
+  username: '',
+  birthdate: '',
+  password: '',
+  confirmPassword: '',
+  hobbies: ''
+}); 
+  this.hobbyArr = [];
+    this.cdr.markForCheck();
   }
 }
